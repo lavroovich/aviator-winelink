161f0fec9529819856e83dea910da2e2079dcb33
lavroovich <168745434+lavroovich@users.noreply.github.com>
2026-01-10 13:03:09 +0700

    сток обновился

---
+++ new arrival/mamain.py
@@ -0,0 +1,18 @@
+import os
+import fitz  # PyMuPDF
+from PIL import Image
+
+folder = r"c:\Users\ivanb\Desktop\papka\code\aviator-winelink\new arrival"
+
+for file in os.listdir(folder):
+    if file.lower().endswith(".pdf"):
+        path = os.path.join(folder, file)
+        name = os.path.splitext(file)[0]
+        doc = fitz.open(path)
+
+        for i, page in enumerate(doc):
+            pix = page.get_pixmap()
+            img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
+            out = os.path.join(folder, f"{name}.webp")
+            img.save(out, "WEBP", quality=90)
+            print(f"✔ {out}")
